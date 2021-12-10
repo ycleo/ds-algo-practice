9e01af6789b689960470b9445f527b396be1c022
ycleo <leoyclee@gmail.com>
2021-12-10 15:04:58 +0800

    q4 js brute sol

---
+++ q4-typed-out-strings/q4-brute.js
@@ -0,0 +1,26 @@
+const buildStr = function (str) {
+    const builtArr = [];
+
+    for (let p = 0; p < str.length; p++) {
+        if (str[p] === '#')
+            builtArr.pop();
+        else
+            builtArr.push(str[p]);
+    }
+    return builtArr;
+} 
+
+const backspaceCompare = function (str1, str2) {
+    const string1 = buildStr(str1);
+    const string2 = buildStr(str2);
+
+    if (string1.length != string2.length) 
+        return false;
+    
+    for (let p = 0; p < string1.length; p++) {
+        if (string1[p] != string2[p]) 
+            return false;
+    }
+
+    return true;
+}
