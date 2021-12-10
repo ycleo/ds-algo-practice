03e09a8ffd7bee1fde2c4fb0bda6134b3c5600d2
ycleo <leoyclee@gmail.com>
2021-12-10 21:58:01 +0800

    q4 js improved sol

--- q4-typed-out-strings/q4-brute.js
+++ q4-typed-out-strings/q4-brute.js
@@ -14,13 +14,16 @@ const backspaceCompare = function (str1, str2) {
     const string1 = buildStr(str1);
     const string2 = buildStr(str2);
 
-    if (string1.length != string2.length) 
+    if (string1.length !== string2.length) 
         return false;
     
     for (let p = 0; p < string1.length; p++) {
-        if (string1[p] != string2[p]) 
+        if (string1[p] !== string2[p]) 
             return false;
     }
 
     return true;
 }
+
+// time: O(n + m) 
+// space: O(n + m)
---
+++ q4-typed-out-strings/q4-improved.js
@@ -0,0 +1,41 @@
+const backspaceCompare = function (s, t) {
+    let p1 = s.length - 1, p2 = t.length - 1;
+
+    while (p1 >= 0 || p2 >= 0) {
+        if (s[p1] === '#' || t[p2] === '#') {
+            if (s[p1] === '#') {
+                let backCount = 2;
+                while (backCount > 0) {
+                    backCount--;
+                    p1--;
+
+                    if(s[p1] === '#')
+                        backCount += 2;
+                }
+            } 
+            if (t[p2] === '#') {
+                if (t[p2] === '#') {
+                    let backCount = 2;
+                    while (backCount > 0) {
+                        backCount--;
+                        p2--;
+
+                        if(t[p2] === '#')
+                            backCount += 2;
+                    }
+                }
+            }
+        } else {
+            if (s[p1] !== t[p2]) {
+                return false;
+            } else {
+              p1--;
+              p2--;  
+            }
+        }
+    }
+    return true;
+}
+
+// time: O(n + m)
+// space: O(1)
