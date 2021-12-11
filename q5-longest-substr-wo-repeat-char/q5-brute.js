4d5d1f41e2cff3e0f9864f4c13c3fd1ab9c2630d
ycleo <leoyclee@gmail.com>
2021-12-11 12:10:26 +0800

    q5 js brute sol

---
+++ q5-longest-substr-wo-repeat-char/q5-brute.js
@@ -0,0 +1,26 @@
+const lengthOfLongestSubstring = function(s) {
+    let longest = 0;
+    
+    for (let i = 0; i< s.length; i++) {
+        let seenChars = {};
+        let currentLength = 0;
+        
+        for (let j = i; j < s.length; j++) {
+            const char = s[j];
+            
+            if (!seenChars[char]) {
+                currentLength++;
+                seenChars[char] = true;
+            } else {
+                break;
+            }
+        }
+        
+        longest = Math.max(longest, currentLength);
+    }
+    
+    return longest;   
+};
+
+// time: O(n^2)
+// space: O(n)
