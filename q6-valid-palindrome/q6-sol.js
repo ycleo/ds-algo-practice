a26f061f26ed9575e7ad405fb55cea5bcfbad881
ycleo <leoyclee@gmail.com>
2021-12-14 20:56:00 +0800

    q6 js sol

---
+++ q6-valid-palindrome/q6-sol.js
@@ -0,0 +1,28 @@
+const subIsPalindrome = function(s, p1, p2) {
+    while (p1 < p2) {
+        if (s[p1] !== s[p2]) return false;
+        p1++;
+        p2--
+    }
+    return true;
+}
+
+var validPalindrome = function(s) {
+    if (s.length <= 1) return true;
+    
+    //s = s.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    let p1 = 0, p2 = s.length - 1;
+    
+    while (p1 <= p2 - 1) {
+        if (s[p1] !== s[p2]) {
+            return (subIsPalindrome(s, p1+1, p2) || subIsPalindrome(s, p1, p2-1));
+        }
+        p1++;
+        p2--;
+    }
+    
+    return true;
+};
+
+// time: O(n)
+// space: O(1)
