0b981069a0361a404c9883d0ec0c6b29d4cf90d6
ycleo <leoyclee@gmail.com>
2021-12-16 15:40:45 +0800

    q8 js sol

---
+++ q8-flatten-multi-level-doubly-linked-list/q8-sol.js
@@ -0,0 +1,39 @@
+const merge = function (cur) {
+    
+    let tail = cur.child;
+    let nxt = cur.next;
+
+    // find the tail of the child linked list
+    while (tail.next) 
+        tail = tail.next;
+    
+    // flatten the tail node    
+    tail.next = nxt;
+    if (nxt)
+        nxt.prev = tail;
+    
+    // flatten the child node
+    cur.next = cur.child;
+    cur.next.prev = cur;
+    cur.child = null;
+}
+
+const flatten = function (head) {
+    if (!head) 
+        return head;
+    
+    // Traverse the starting linked list
+    let cur = head;
+    while (cur) {
+        if (!cur.child)
+            cur = cur.next;
+        else 
+            // merge the child linked list
+            merge(cur);
+    }
+    
+    return head;
+}
+
+// time: O(n)
+// space: O(1)
