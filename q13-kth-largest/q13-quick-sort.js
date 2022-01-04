364a353e137f39b6b2c212d80545f874e732f5fc
ycleo <leoyclee@gmail.com>
2022-01-04 22:32:48 +0800

    q13 improved the code cleanliness

--- q13-kth-largest/q13-quick-sort.js
+++ q13-kth-largest/q13-quick-sort.js
@@ -1,32 +1,67 @@
 // LeetCode 215.
 // https://leetcode.com/problems/kth-largest-element-in-an-array/
 
-// test cases:
+// - Test Cases:
 // [5, 6, 4, 2, 3, 1], k = 2   =>  5
 // [1, 2, 2, 2, 3, 4], k = 4   =>  2
 // [3], k = 1                  =>  3
 
+// - Quick Sort Solution: divide and conquer => (How to find the partition index)
+//                 r
+// [5, 6, 4, 2, 1, 3]
+//  p
+//  i   ->   i
+//         (2 < 3)
+
+//                     r
+// ["2", 6, 4, "5", 1, 3]
+//   p
+//              i
+
+//                    r
+// [2, "3", 4, 5, 1, "6"]
+//     "p"
+//             i
+
+
+// Algorithm:
+// qs (nums, 0, nums.len - 1)
+
+// qs (arr, left, right):
+//      if (left < right) 
+//          p = partition(arr, left, right)
+//          qs (arr, left, p-1)
+//          qs (arr, p+1, right)
+
+// partition (arr, left, right):
+//      p = i = left
+//      for i from left to right
+//          if (arr[i] < arr[right])
+//              swap arr[j] and arr[j]
+//              p++
+//
+//      swap arr[p] and arr[right]
+//      return p
+
+const swap = function(arr, i, j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+};
+
 const partition = function(arr, left, right) {
     let piv = arr[right];
-    
-    let i = left - 1;
-    for (let j = left; j < right; j++) {
-        if (arr[j] > piv) {
-            i++;
-            // swap arr[i] and arr[j]
-            let temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
+    let partitionIdx = left;
+
+    for (let i = left; i < right; i++) {
+        if (arr[i] > piv) {
+            swap(arr, i, partitionIdx);
+            partitionIdx++;
+        }    
     }
-    i++;
     
-    // swap arr[i] and arr[right]
-    let temp = arr[i];
-    arr[i] = arr[right];
-    arr[right] = temp;
-    
-    return i;
+    swap(arr, partitionIdx, right);
+    return partitionIdx;
 };
 
 const quickSort = function(arr, left, right) {
