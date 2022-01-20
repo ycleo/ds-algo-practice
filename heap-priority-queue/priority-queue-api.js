4cbcfe884be538028246797bcaea9053d65cf4ff
ycleo <leoyclee@gmail.com>
2022-01-20 15:10:22 +0800

    priority queue implementation

---
+++ heap-priority-queue/priority-queue-api.js
@@ -0,0 +1,96 @@
+
+// size; isEmpty; peek; push; pop
+
+class PriorityQueue {
+    constructor(comparator = (a, b) => a > b) {
+        this._heap = [];
+        this._comparator = comparator;
+    }
+
+    // find the heap size
+    size() {
+        return this._heap.length;
+    }
+    
+    isEmpty() {
+        return this._heap.length == 0;
+    }
+
+    peek() {
+        return this._heap[0];
+    }
+
+    _parent(idx) {
+        return Math.floor((idx - 1) / 2);
+    }
+
+    _leftChild(idx) {
+        return idx * 2 + 1;
+    }
+
+    _rightChild(idx) {
+        return idx * 2 + 2;
+    }
+
+    _compare(i, j) {
+        return this._comparator(this._heap[i], this._heap[j]);
+    }
+
+    _swap(i, j) {
+        [this._heap[i], this._heap[j]] = [this._heap[j], this._heap[i]];
+    }
+
+    push(val) {
+        this._heap.push(val);
+        this._compareUp();
+        return this.size();
+    }
+
+    _compareUp() {
+        let nodeIdx = this.size() - 1;
+
+        while(0 < nodeIdx && this._compare(nodeIdx, this._parent(nodeIdx))) {
+            this._swap(nodeIdx, this._parent(nodeIdx));
+            nodeIdx = this._parent(nodeIdx);
+        }
+    }
+
+    pop() {
+        if(this.size() > 1)
+            this._swap(0, this.size() - 1);
+
+        const popNode = this._heap.pop();
+        this._compareDown();
+        return popNode;
+    }
+    _compareDown() {
+        let nodeIdx = 0;
+        while (
+            (this._leftChild(nodeIdx) < this.size() 
+             && this._compare(this._leftChild(nodeIdx), nodeIdx))
+            || (this._rightChild(nodeIdx) < this.size()
+             && this._compare(this._rightChild(nodeIdx), nodeIdx))
+            ) {
+                const greaterChild = 
+                    this._leftChild(nodeIdx) < this.size()
+                    && this._compare(this._leftChild(nodeIdx), this._rightChild(nodeIdx))
+                    ? this._leftChild(nodeIdx)
+                    : this._rightChild(nodeIdx);
+                
+                this._swap(greaterChild, nodeIdx);
+                nodeIdx = greaterChild;
+        }
+    }
+}
+
+const pq = new PriorityQueue();
+pq.push(15);
+pq.push(12);
+pq.push(50);
+pq.push(7);
+pq.push(40);
+pq.push(22);
+
+while(!pq.isEmpty()) {
+  console.log(pq.pop());
+}
